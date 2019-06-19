f9ccccb7f5e22bb9ddd5e060187999406beb231e
petern160 <peternguyen160@gmail.com>
2019-06-18 17:48:16 -0700

    can destroy ships on click

--- src/scenes/Scene1.js
+++ src/scenes/Scene1.js
@@ -2,9 +2,11 @@
 /* eslint-disable class-methods-use-this */
 import Phaser from "phaser";
 import background from "../assets/background.png";
-import ship from "../assets/ship.png";
-import ship2 from "../assets/ship2.png";
-import ship3 from "../assets/ship3.png";
+import ship from "../assets/s1sprite.png";
+import ship2 from "../assets/s2sprite.png";
+import ship3 from "../assets/s3sprite.png";
+import explosion from "../assets/explosion.png";
+
 
 class Scene1 extends Phaser.Scene {
   constructor() {
@@ -13,9 +15,23 @@ class Scene1 extends Phaser.Scene {
 
   preload() {
     this.load.image('background', background)
-    this.load.image('ship', ship)
-    this.load.image('ship2', ship2)
-    this.load.image('ship3', ship3)
+    this.load.spritesheet('ship', ship, {
+      frameWidth: 16,
+      frameHeight: 16,
+    })
+    this.load.spritesheet('ship2', ship2, {
+      frameWidth: 32,
+      frameHeight: 16,
+    })
+    this.load.spritesheet('ship3', ship3, {
+      frameWidth: 32,
+      frameHeight: 32,
+    })
+    this.load.spritesheet('explosion', explosion, {
+      frameWidth: 16,
+      frameHeight: 16,
+    })
+   
   }
    
   create() {
--- src/scenes/Scene2.js
+++ src/scenes/Scene2.js
@@ -18,17 +18,66 @@ class Scene1 extends Phaser.Scene {
   
   create() {
       // declare class var to access inside the class 
-    this.background = this.add.image(0,0, 'background')
+      // adds tilesprite instead of image for moving background
+    this.background = this.add.tileSprite(0,0, width, height, 'background')
     // set background origin to top left 
     this.background.setScale(2)
     this.background.setOrigin(0,0)
 
-    this.ship1 = this.add.image(width/2 -50, height/2, "ship")
+    this.ship1 = this.add.sprite(width/2 -50, height/2, "ship")
     this.ship1.setScale(1.5)
-    this.ship2 = this.add.image(width/2, height/2, "ship2")
+    this.ship2 = this.add.sprite(width/2, height/2, "ship2")
     this.ship2.setScale(1.5)
-    this.ship3 = this.add.image(width/2 + 50, height/2, "ship3")
+    this.ship3 = this.add.sprite(width/2 + 50, height/2, "ship3")
     this.ship3.setScale(1.5)
+
+    this.anims.create({
+        // animation name
+        key:'ship1_anim',
+        // frames from ship spritesheet
+        frames: this.anims.generateFrameNumbers("ship"),
+        frameRate: 20,
+        // -1 infinite loop
+        repeat:-1
+    })
+
+    this.anims.create({
+        
+        key:'ship2_anim',
+        frames: this.anims.generateFrameNumbers("ship2"),
+        frameRate: 20,
+        repeat:-1
+    })
+
+    this.anims.create({
+        
+        key:'ship3_anim',
+        frames: this.anims.generateFrameNumbers("ship3"),
+        frameRate: 20,
+        repeat:-1,
+   
+    })
+
+    this.anims.create({
+        
+        key:'explode',
+        frames: this.anims.generateFrameNumbers("explosion"),
+        frameRate: 20,
+        repeat:0,
+        // disappears once completed
+        hideOnComplete: true
+    })
+
+    this.ship1.play("ship1_anim");
+    this.ship2.play("ship2_anim");
+    this.ship3.play("ship3_anim");
+
+    this.ship1.setInteractive();
+    this.ship2.setInteractive();
+    this.ship3.setInteractive();
+
+    // on click down does call back of destroy ship then passes in scope of this
+    this.input.on('gameobjectdown', this.destroyShip, this)
     
     this.add.text(20, 20, 'PLAYING GAME', {font: "25px Arial", fill: "yellow"});
   
@@ -49,10 +98,17 @@ class Scene1 extends Phaser.Scene {
       ship.x = randomX;
   }
 
+  destroyShip(pointer, gameObject){
+      gameObject.setTexture("explosion");
+      gameObject.play("explode")
+  }
+
   update() {
-      this.moveShip(this.ship1, 1)
-      this.moveShip(this.ship2, 2)
-      this.moveShip(this.ship3, 3)
+      this.moveShip(this.ship1, 2)
+      this.moveShip(this.ship2, 2.5)
+      this.moveShip(this.ship3, 3.3)
+      // changes texture backgorund for moving effect
+      this.background.tilePositionY -= 0.5;
   }
 }
 
